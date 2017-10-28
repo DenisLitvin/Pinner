ab6473eeda83a3bacd67e03218e623a0b25c4aac
Denis <den.litvinn@gmail.com>
2017-10-28 22:37:01 +0300

    bug fixes

---
+++ Example/Pods/Target Support Files/Pinner/Pinner-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pinner : NSObject
+@end
+@implementation PodsDummy_Pinner
+@end
---
+++ Example/Pods/Target Support Files/Pods-Pinner_Tests/Pods-Pinner_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Pinner_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_Pinner_Tests
+@end
